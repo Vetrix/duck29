6cd922dee9c44fa93b8e83f117ec461351f05914
Dzannun Austi <34627197+Vetrix@users.noreply.github.com>
2019-04-01 16:37:07 +0700

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python duck29.py
---
+++ linepy/__init__.py
@@ -0,0 +1,9 @@
+from .client import LINE
+from .channel import Channel
+from .oepoll import OEPoll
+from akad.ttypes import OpType
+
+__modified__        = 'Zero Cool'
+__url__             = 'http://github.com/fadhiilrachman/line-py'
+
+__all__ = ['LINE', 'Channel', 'OEPoll', 'OpType', '__modified__']
